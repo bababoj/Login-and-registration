5c9e108bad46b2546719deaf6469cde2b0ff5303
Arina Nekrasova <98659422+nebababoy@users.noreply.github.com>
2023-10-03 02:36:03 +0200

    commit2

--- Frames/LogInFrame.java
+++ Frames/LogInFrame.java
@@ -4,6 +4,10 @@ import lombok.Getter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class LogInFrame extends JFrame {
 
@@ -11,9 +15,10 @@ public class LogInFrame extends JFrame {
     private JLabel userlabel;
     private JLabel passlabel;
     private JTextField textFieldUser;
-    private JTextField textFieldPass;
+    //  private JTextField textFieldPass;
+    private JPasswordField passField;
     @Getter
-    private JButton registration;
+    private JButton signUp;
     @Getter
     private JButton login;
 
@@ -21,7 +26,17 @@ public class LogInFrame extends JFrame {
     private JPanel contentPanel;
     private JPanel colorStripe;
 
+    private JPanel inputPanel;
+
     public LogInFrame(){
+
+        super("Sign In");
+        Font customFont = null;
+        try {
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Albert.ttf")).deriveFont(Font.PLAIN, 50);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(500, 500);
@@ -36,6 +51,91 @@ public class LogInFrame extends JFrame {
 
         contentPanel = new JPanel();
         contentPanel.setBackground(Color.WHITE);
+
+        // contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+      //  contentPanel.setLayout(new GridLayout(7, 1));
+        nameFrame = new JLabel("LOGIN");
+        nameFrame.setFont(customFont);
+       // nameFrame.setBounds(100, 300, 100, 30);
+        contentPanel.add(nameFrame, BorderLayout.NORTH);
+
+
+        inputPanel = new JPanel();
+        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+        inputPanel.setBackground(Color.WHITE);
+        inputPanel.add(Box.createVerticalStrut(45));
+
+
+        userlabel = new JLabel("Username: ");
+        assert customFont != null;
+        userlabel.setFont(customFont.deriveFont(25f));
+        userlabel.setForeground(Color.DARK_GRAY);
+        inputPanel.add(userlabel);
+
+        textFieldUser = new JTextField();
+        textFieldUser.setPreferredSize(new Dimension(300, 30));
+        textFieldUser.setFont( textFieldUser.getFont().deriveFont(20f));
+        inputPanel.add(textFieldUser);
+        inputPanel.add(Box.createVerticalStrut(60));
+
+        passlabel = new JLabel("Password: ");
+        passlabel.setFont(customFont.deriveFont(25f));
+        passlabel.setForeground(Color.DARK_GRAY);
+        inputPanel.add(passlabel);
+
+        //textFieldPass = new JTextField();
+        passField = new JPasswordField();
+        passField.setPreferredSize(new Dimension(300, 30));
+        passField.setFont( textFieldUser.getFont().deriveFont(20f));
+        passField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Получаем введенный пароль и преобразуем его в строку со звездочками
+                char[] passwordChars = passField.getPassword();
+                String password = new String(passwordChars);
+                String maskedPassword = new String(new char[passwordChars.length]).replace('\0', '*');
+
+                // Выводим звездочки вместо пароля
+                System.out.println(maskedPassword);
+
+                // Здесь можно добавить код для обработки пароля
+            }
+        });
+
+        inputPanel.add(passField);
+        inputPanel.add(Box.createVerticalStrut(60));
+
+        contentPanel.add(inputPanel);
+
+        login = new JButton("Login");
+        login.setFocusable(false);
+        login.setBackground(Color.PINK);
+        login.setFont(customFont.deriveFont(20f));
+        contentPanel.add(login,  BorderLayout.SOUTH);
+
+        signUp = new JButton("Sign Up");
+
+        signUp.setFocusable(false);
+        signUp.setBackground(Color.PINK);
+        signUp.setFont(customFont.deriveFont(20f));
+        contentPanel.add(signUp,  BorderLayout.SOUTH);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         mainPanel.add(contentPanel, BorderLayout.CENTER);
